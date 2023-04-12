172e68c68772df47704991690ed37d0cd55c0495
RybakovAlexey <rybakov@r-tech.ru>
2023-04-12 23:06:38 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/models/ChildControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1.models
+{
+    internal class ChildControl: UserControl
+    {
+        public static readonly DependencyProperty PropertyProperty = DependencyProperty.Register(
+        nameof(Property), typeof(string), typeof(ChildControl), new PropertyMetadata(default(string)));
+        public string Property
+
+        {
+            get { return (string)GetValue(PropertyProperty); }
+            set { SetValue(PropertyProperty, value); }
+        }
+
+
+
+        public ChildControl()
+        {
+            SetBinding(PropertyProperty, new Binding(nameof(ParentControl.Property)) { Mode = BindingMode.OneWay });
+        }
+
+    }
+}
---
+++ WpfApp1/models/ParentControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp1.models
+{
+    internal class ParentControl: UserControl, INotifyPropertyChanged
+    {
+        private string _property;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Property
+        {
+            get { return _property; }
+            set
+            {
+                _property = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Property)));
+            }
+        }
+        public ParentControl()
+        {
+
+            DataContext = this;
+        }
+    }
+}
